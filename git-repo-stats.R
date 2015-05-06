f1628c475c76bb29185082a789e622fdddd795da
Manuel de la Peña <manuel.delapenya@liferay.com>
2015-05-06 08:53:01 +0200

    Create R file to plot the series

---
+++ git-repo-stats.R
@@ -0,0 +1,14 @@
+install.packages("ggplot2")
+
+# 
+commits <- read.csv("/tmp/commits-stats.csv", header = TRUE, sep = " ")
+
+x <- seq(1, nrow(commits), 1)
+
+df <- data.frame(x, commits$addedLines, commits$deletedLines, commits$modifiedFiles)
+
+revdf <- df[rev(rownames(df)),]
+
+library("ggplot2")
+
+ggplot (revdf, aes(x, y=value, color=variable)) + geom_line(aes(y=commits$addedLines, col="Added Lines")) + geom_line(aes(y=commits$deletedLines, col="Deleted Lines")) + geom_line(aes(y=commits$modifiedFiles, col="Modified Files"))
